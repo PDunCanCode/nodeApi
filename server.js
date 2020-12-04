3e580ac42f87292ecd38c1a4e8b8c11fc9901569
pDunCanCode <chesiredarling@gmail.com>
2020-12-04 14:47:25 -0800

    first routes

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.post("api/v1/bootcamps");
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
