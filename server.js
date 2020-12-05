41280b478f3378b9b0d90652c076d06ee8d85edc
pDunCanCode <chesiredarling@gmail.com>
2020-12-05 08:16:56 -0800

    update middleware to morgan

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
+const morgan = require("morgan");
 
 const bootcamps = require("./routes/bootcamps");
 //Load env variables
@@ -8,7 +8,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(logger);
+//Dev logging middlewar
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use("/api/v1/bootcamps", bootcamps);
 
