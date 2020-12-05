9021e038eab309bd4442548606268a14d311354e
pDunCanCode <chesiredarling@gmail.com>
2020-12-05 14:04:24 -0800

    add models/bootcamp schema

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
-const bootcamps = require("./routes/bootcamps");
 //Load env variables
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -17,7 +22,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`ServerRunning in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `ServerRunning in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 );
+
+//Handle unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Unhandled rejection: ${err.message}`.red);
+  //Close Server exit Process
+  server.close(() => process.exit(1));
+});
