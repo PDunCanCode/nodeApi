14792b9e32feb5c7e0310e4d2cebe034555b14c4
pDunCanCode <chesiredarling@gmail.com>
2020-12-06 21:23:17 -0800

    add geocoding

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -110,4 +110,27 @@ const BootcampSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
-module.expots = mongoose.model("Bootcamp", BootcampSchema);
+
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode and create Location Field
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+  this.address = undefined;
+  next();
+});
+
+module.exports = mongoose.model("Bootcamp", BootcampSchema);
