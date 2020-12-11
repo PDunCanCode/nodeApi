9c3e6222ac0a237dd445b7b01219adc329996e53
pDunCanCode <chesiredarling@gmail.com>
2020-12-11 15:28:20 -0800

    add auth and jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,5 +3,17 @@ const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  //Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
