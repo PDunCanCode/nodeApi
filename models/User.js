888fd625492ab508d0f0e5a42d391a7dd605de4a
pDunCanCode <chesiredarling@gmail.com>
2020-12-13 11:25:11 -0800

    add forgotPassword auth

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -49,10 +50,13 @@ const UserSchema = new mongoose.Schema({
 //encrypt password with bcrypt
 
 UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
-
 // Sign JWT and return
 
 UserSchema.methods.getSignedJwtToken = function () {
@@ -64,6 +68,23 @@ UserSchema.methods.getSignedJwtToken = function () {
 // Match user enterd Password to hashed passwrod in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 module.exports = mongoose.model("User", UserSchema);
