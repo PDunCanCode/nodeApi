671edccbf5b9f96fc0a94e934e2d135af0e72c6c
pDunCanCode <chesiredarling@gmail.com>
2020-12-13 09:52:42 -0800

    update routes awith fixes for variables and user permissions

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -25,12 +26,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
+
 //Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Courses.create(courses);
-
+    await users.create(users);
     console.log("Data IMPORTED".green.inverse);
     process.exit;
   } catch (err) {
@@ -43,7 +48,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
-
+    await User.deleteMany();
     console.log("Data DELETED".red.inverse);
     process.exit;
   } catch (err) {
